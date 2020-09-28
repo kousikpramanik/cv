d4ccd106d09218ffcc6689ed970fa3364658862c
kousikpramanik <66074903+kousikpramanik@users.noreply.github.com>
2020-09-28 23:13:59 +0530

    Add files via upload

--- services.php
+++ services.php
@@ -1,23 +1,27 @@
 <?php
-if(isset($_POST['submit'])){
-    $to = "kousikpramanik539@gmail.com"; // this is your Email address
-    $from = $_POST['exampleInputEmail1']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['exampleFormControlTextarea1'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['exampleFormControlTextarea1'];
+    if(isset($_POST['submit'])) {
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    $headers  = "MIME-Version: 1.0\r\n";
-    $headers .= "Content-type: text/html; charset=UTF-8\r\n";
-    $headers .= "From: <".$from. ">" ;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
+    $name = $_POST['nome'];
+    $email = $_POST['exampleInputEmail'];
+    $message = $_POST['exampleFormControlTextarea1'];
+
+    $to = "kousikpramanik@gmail.com";
+    $subject = "Site contact form";
+
+    $header = "From: ".$exampleInputEmail."\r\n";
+    $header .= "Cc: ".$exampleInputEmail."\n";
+    $header .= "Reply-To : ".$exampleInputEmail."\r\n";
+    $header .= "Return-Path : ".$exampleInputEmail."\r\n";
+    $header .= "X-Mailer: PHP\r\n";
+    $header .= "MIME-Version: 1.0\r\n";
+    $header .= "Content-Type: text/plain; charset=iso-8859-1\r\n";
+
+    if(mail($to, $subject, $message, $header))
+    {
+      echo "Mail Sent Successfully";
+    }else{
+      echo "Mail Not Sent";
     }
-?>
+
+    }
+    ?>
