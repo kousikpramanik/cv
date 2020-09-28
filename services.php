54700d9f71005f67f1942ad1b8757c84ede5e4bb
kousikpramanik <66074903+kousikpramanik@users.noreply.github.com>
2020-09-28 22:02:56 +0530

    Add files via upload

---
+++ services.php
@@ -0,0 +1,4 @@
+<?php
+    mail('kousikpramanik539@gmail.com', $_POST['first_name'], $_POST['last_name'], $_POST['exampleInputEmail1'], $_POST['exampleFormControlTextarea1']);
+?>
+<p>Your Service Request Has Been Submitted.</p>
