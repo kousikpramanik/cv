feb5fd26afcc8399d106516eb6fe0a807e1f790e
kousikpramanik <66074903+kousikpramanik@users.noreply.github.com>
2020-09-28 22:21:21 +0530

    Add files via upload

--- services.php
+++ services.php
@@ -1,4 +1,20 @@
 <?php
-    mail('kousikpramanik539@gmail.com', $_POST['first_name'], $_POST['last_name'], $_POST['exampleInputEmail1'], $_POST['exampleFormControlTextarea1']);
+if(isset($_POST['submit'])){
+    $to = "kousikpramanik539@gmail.com"; // this is your Email address
+    $from = $_POST['exampleInputEmail1']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['exampleFormControlTextarea1'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['exampleFormControlTextarea1'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
 ?>
-<p>Your Service Request Has Been Submitted.</p>
