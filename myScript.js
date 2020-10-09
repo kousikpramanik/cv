d59d569a0368fef513c95b2691146fa319000cd0
kousikpramanik <66074903+kousikpramanik@users.noreply.github.com>
2020-10-09 19:28:16 +0530

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,4 +1,6 @@
 function myFunction() {
   window.alert("This is a static web page. I will back very soon with dynamic website. Please wait!.");
-
+}
+function myFunction2(){
+  window.alert("Please copy the mail address & sent a mail in the given mail address.")
 }
